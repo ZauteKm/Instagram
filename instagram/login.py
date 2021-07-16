c5c5a67c93fcc5b6ee6c6cbca9506bed3ba48e00
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-07-16 07:03:28 +0530

    Update login.py

--- instagram/login.py
+++ instagram/login.py
@@ -41,15 +41,16 @@ async def login(bot, message):
 			reply_markup=InlineKeyboardMarkup(
 				[
 					[
-						InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-						InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+						InlineKeyboardButton("👨🏼‍💻 Developer", url='https://t.me/c/1417456571/588'),
+						InlineKeyboardButton("Bots Project 🤖", url="https://t.me/c/1481808444/141"),
+                        
 					],
                     [
-                        InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-						InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
+                        InlineKeyboardButton("🌐 Source Code", url="https://github.com/ZauteKm/InstagramBot"),
+						InlineKeyboardButton("Deploy Own Bot ✅", url="https://heroku.com/deploy?template=https://github.com/ZauteKm/InstagramBot")
                     ],
                     [
-                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#subin")
+                        InlineKeyboardButton("❇️ Help & Commands ❇️", callback_data="help#zaute")
 
                     ]
 					
@@ -169,15 +170,16 @@ async def logout(bot, message):
 			reply_markup=InlineKeyboardMarkup(
 				[
 					[
-						InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-						InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+						InlineKeyboardButton("👨🏼‍💻 Developer", url='https://t.me/c/1417456571/588'),
+						InlineKeyboardButton("Bots Project 🤖", url="https://t.me/c/1481808444/141"),
+                        
 					],
                     [
-                        InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-						InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
+                        InlineKeyboardButton("🌐 Source Code", url="https://github.com/ZauteKm/InstagramBot"),
+						InlineKeyboardButton("Deploy Own Bot ✅", url="https://heroku.com/deploy?template=https://github.com/ZauteKm/InstagramBot")
                     ],
                     [
-                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#subin")
+                        InlineKeyboardButton("❇️ Help & Commands ❇️", callback_data="help#zaute")
 
                     ]
 					
