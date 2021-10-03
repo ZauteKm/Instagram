5d4b062b8e78068dc55beb5e4b3ed66ac3fbba33
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-03 12:54:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -188,12 +188,12 @@ async def upload(m, bot, chat_id, dir):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-					InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+					InlineKeyboardButton("👨🏼‍💻 Developer", url='https://t.me/zautekm'),
+					InlineKeyboardButton("Other Bots 🤖", url="https://t.me/BotzListBot")
 				],
 				[
-					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/subin_works")
+					InlineKeyboardButton("🔰 Source Code", url="https://github.com/ZauteKm/Instagram"),
+                    InlineKeyboardButton("Channel 📢", url="https://t.me/TGBotsProJect")
 				]
 			]
 			)
